057a4cd2fd8f3956a90fa09f696ca246a8361533
Pawel Paszki <pawelpaszki@gmail.com>
2018-03-05 15:17:00 +0000

    clean master commit before the deveopment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'new application';
+  public title = 'new application';
 }
